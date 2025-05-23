1a786014ba658172c4a990ee1f61c67ed7d70e72
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-05-23 09:11:22 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 
-
-## Understanding How It Works
+### Understanding How It Works
 
 ![alt text](image.png)
 
@@ -8,3 +7,10 @@
 2. An async task is spawned, which prints "Jordan's Komputer: howdy!" and then waits for a 2-second timer.
 3. The executor runs, sees the task is waiting, and yields.
 4. After 2 seconds, the timer completes, the task resumes, and prints "Jordan's Komputer: done!".
+
+### Multiple Spawn and Removing Drop
+
+![alt text](image-1.png)
+
+The order of "howdy" messages is always the same (spawn order), but the "done" messages can appear in any order after the timers complete, depending on task scheduling. This is a trait of using async executors to explicitly control task order.
+
--- src/main.rs
+++ src/main.rs
@@ -105,11 +105,21 @@ fn main() {
         println!("Jordan's Komputer: done!");
     });
 
+    spawner.spawn(async {
+        println!("Jordan's Komputer: howdy2!");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Jordan's Komputer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("Jordan's Komputer: howdy3!");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Jordan's Komputer: done3!");
+    });
+
     println!("Jordan's Komputer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
-
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
     executor.run();
